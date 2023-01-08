f810a3fb5e96da1bac25c48a02be703c0156b8ff
Arthur Bayr <arthur.holz.91@gmail.com>
2023-01-08 16:52:41 +0100

    wording

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import prometheus_client
 
 from config import topics
 
-connectors = dict()
+collectors = dict()
 
 
 def setup_collectors():
@@ -27,19 +27,19 @@ def setup_collectors():
             g.labels(metric=metric, device_id=device_id, topic=topic)
         else:
             raise Exception(f"Unknown topic length: {topic}")
-        connectors[topic] = g
+        collectors[topic] = g
 
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-    for topic, gauge in connectors.items():
+    for topic, gauge in collectors.items():
         client.subscribe(topic)
         print("Subscribed to", topic)
 
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
-    connector = connectors.get(msg.topic)
+    connector = collectors.get(msg.topic)
     connector.set(msg.payload)
 
 
